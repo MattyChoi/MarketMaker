debc4d9aaa557dbfac447be28e52e9abd7e5de9a
Gustav Baumgart <baumg260@umn.edu>
2022-05-29 18:41:36 -0500

    Added grid layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,72 +25,66 @@ function App(props) {
 
   return (
     <div className="App">
-      {/* column where you put */}
-      <div id="placeOrder">
-        <form onSubmit={placeOrder}>
-          <label>
-            ID:
-            <input type="text" name="id" value={orderVals.id} onChange={handleChange}/>
-          </label>
-          <label>
-            Side:
-            <select name="side" id="side" value={orderVals.side} onChange={handleChange}>
-              <option value="buy">BUY</option>
-              <option value="sell">SELL</option>
-            </select>
-          </label>
-          <label>
-            Order Type:
-            <select name="type" id="type" value={orderVals.type} onChange={handleChange}>
-              <option value="limit">Limit Order</option>
-              <option value="market">Market Order</option>
-            </select>
-          </label>
-          <label>
-            Price:
-            <input type="text" name="price" value={orderVals.price} onChange={handleChange}/>
-          </label>
-          <label>
-            Volume:
-            <input type="text" name="volume" value={orderVals.volume} onChange={handleChange}/>
-          </label>
-          <input type="submit" value="Submit"/>
-        </form>
-
-      </div>
-
-      {/* column where the information is  */}
-      <div id="ownOrders">
-        
-        <div id="activeOrders" name="activeOrders">
-          Active Orders
+      <div className="body">
+        <div id="placeOrder" className="column">
+          <form>
+            <label className="row">
+              ID:
+              <input type="text" name="id" value={orderVals.id} onChange={handleChange}/>
+            </label>
+            <label className="row">
+              Side:
+              <select name="side" id="side" value={orderVals.side} onChange={handleChange}>
+                <option value="buy">BUY</option>
+                <option value="sell">SELL</option>
+              </select>
+            </label>
+            <label className="row">
+              Order Type:
+              <select name="type" id="type" value={orderVals.type} onChange={handleChange}>
+                <option value="limit">Limit Order</option>
+                <option value="market">Market Order</option>
+              </select>
+            </label>
+            <label className="row">
+              Price:
+              <input type="text" name="price" value={orderVals.price} onChange={handleChange}/>
+            </label>
+            <label className="row">
+              Volume:
+              <input type="text" name="volume" value={orderVals.volume} onChange={handleChange}/>
+            </label>
+            <input type="submit" value="Submit" className="row"/>
+          </form>
         </div>
-
-        <div id="positions" name="positions">
-          Positions: 0
-        </div>
-
-      </div>
-
-      {/* column where you put  */}
-      <div id="allOrders">
-        
-        
-        <label>
-          Order Book
-          <div id="orderBook" name="orderBook">
-            Asks:<br></br>
-            Bids:<br></br>
+        <div id="ownOrders" className="column">
+          
+          <div id="activeOrders" name="activeOrders" className="row">
+            Active Orders
           </div>
-        </label>
-        <input type="submit" value="Delete All Orders"/>
-        <form>
-          <label>
-              Delete by Order ID:
-              <input type="text" name="delete"/>
+
+          <div id="positions" name="positions"  className="row">
+            Positions: 0
+          </div>
+
+        </div>
+        <div id="allOrders" className="column">
+          <label className="row">
+            Order Book
+            <div id="orderBook" name="orderBook">
+              Asks:<br></br>
+              Bids:<br></br>
+            </div>
           </label>
-          <input type="submit" value="Delete Order"/>
-        </form>
+          <input type="submit" value="Delete All Orders" className="row"/>
+          <form>
+            <label className="row">
+                Delete by Order ID:
+                <input type="text" name="delete"/>
+            </label>
+            <input type="submit" value="Delete Order" className="row"/>
+          </form>
+        </div>
       </div>
     
     </div>
