8af7c6116295a5c8ce907678e292d8c78c1145ee
Matthew Choi <matthew.choi02@gmail.com>
2022-05-29 14:55:12 -0700

    created html elements for ordering

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* column where you put  */}
+      {/* column where you put */}
       <div id="placeOrder">
+        <form>
+          <label>
+            ID:
+            <input type="text" name="id"/>
+          </label>
+          <label>
+            Side:
+            <select name="side" id="side">
+              <option value="buy side">BUY</option>
+              <option value="sell side">SELL</option>
+            </select>
+          </label>
+          <label>
+            Order Type:
+            <select name="side" id="side">
+              <option value="limit">Limit Order</option>
+              <option value="market">Market Order</option>
+            </select>
+          </label>
+          <label>
+            Price:
+            <input type="text" name="price"/>
+          </label>
+          <label>
+            Volume:
+            <input type="text" name="volume"/>
+          </label>
+          <input type="submit" value="Submit"/>
+        </form>
 
       </div>
 
-      {/* column where the information is  */}
-      <div id="ownOrders">
-        
-      </div>
-
-      {/* column where you put  */}
-      <div id="allOrders">
-        
-      </div>
-    
     </div>
   );
 }
