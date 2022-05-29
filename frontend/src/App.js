fe450977613f14dd50f359140d6718bee3927110
Matthew Choi <matthew.choi02@gmail.com>
2022-05-29 16:16:41 -0700

    added states for the order fields

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,58 @@
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function App(props) {
+
+  const [orderVals, setOrderVals] = useState({
+    id: "", 
+    side: "buy",
+    type: "Limit Order",
+    price: 0,
+    volume: 0,
+  });
+
+  function handleChange(e) {
+      const { name, value } = e.target;
+      setOrderVals(prev => ({
+          ...prev,
+          [name]: value
+      }));
+  };
+
+  function placeOrder(e) {
+    console.log(orderVals);
+  }
+
   return (
     <div className="App">
       {/* column where you put */}
       <div id="placeOrder">
-        <form>
+        <form onSubmit={placeOrder}>
           <label>
             ID:
-            <input type="text" name="id"/>
+            <input type="text" name="id" value={orderVals.id} onChange={handleChange}/>
           </label>
           <label>
             Side:
-            <select name="side" id="side">
-              <option value="buy side">BUY</option>
-              <option value="sell side">SELL</option>
+            <select name="side" id="side" value={orderVals.side} onChange={handleChange}>
+              <option value="buy">BUY</option>
+              <option value="sell">SELL</option>
             </select>
           </label>
           <label>
             Order Type:
-            <select name="side" id="side">
+            <select name="type" id="type" value={orderVals.type} onChange={handleChange}>
               <option value="limit">Limit Order</option>
               <option value="market">Market Order</option>
             </select>
           </label>
           <label>
             Price:
-            <input type="text" name="price"/>
+            <input type="text" name="price" value={orderVals.price} onChange={handleChange}/>
           </label>
           <label>
             Volume:
-            <input type="text" name="volume"/>
+            <input type="text" name="volume" value={orderVals.volume} onChange={handleChange}/>
           </label>
           <input type="submit" value="Submit"/>
         </form>
@@ -44,7 +66,7 @@ function App() {
           Active Orders
         </div>
 
-        <div id="positions" name="positions" readonly>
+        <div id="positions" name="positions">
           Positions: 0
         </div>
 
